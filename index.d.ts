4a3306bcf469035b3082f85e15f67e6497aa5103
mattco98 <matthewcolsson@gmail.com>
2019-08-31 18:19:18 -0500

    Fix typings

--- index.d.ts
+++ index.d.ts
@@ -1,16 +1,43 @@
 declare module 'for-editor' {
+  export interface IToolbar {
+    h1?: boolean;
+    h2?: boolean;
+    h3?: boolean;
+    h4?: boolean;
+    h5?: boolean;
+    h6?: boolean;
+    bold?: boolean;
+    italic?: boolean;
+    underline?: boolean;
+    strikethrough?: boolean;
+    img?: boolean;
+    link?: boolean;
+    code?: boolean;
+    preview?: boolean;
+    expand?: boolean;
+    undo?: boolean;
+    redo?: boolean;
+    save?: boolean;
+    subfield?: boolean;
+  }
+
   interface IP {
-    value?: string
-    placeholder?: string
-    lineNum?: string
-    style?: any
-    height?: string
-    preview?: boolean
-    expand?: boolean
-    subfield?: boolean
-    language?: string
-    toolbar?: any
+    value?: string;
+    lineNum?: number;
+    onChange?: (value: string) => void;
+    onSave?: (value: string) => void;
+    placeholder?: string;
+    fontSize?: string;
+    disabled?: boolean;
+    style?: object;
+    height?: string;
+    preview?: boolean;
+    expand?: boolean;
+    subfield?: boolean;
+    toolbar?: IToolbar;
+    language?: string;
+    addImg?: (file: File, index: number) => void;
   }
-  export default class ForEditor extends React.Component<IP, any> {
-  }
+
+  export default class ForEditor extends React.Component<IP, any> { }
 }
